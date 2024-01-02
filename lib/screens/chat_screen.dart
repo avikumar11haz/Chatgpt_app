4446ccec8f67fdb24c167e67be9245302b674611
avikumarr355@gmail.com <avikumarr355@gmail.com>
2024-01-03 00:59:41 +0530

    implement the ModalBottomSheet

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:chatgpt_app/constants/constants.dart';
 import 'package:chatgpt_app/services/assets_manager.dart';
 import 'package:chatgpt_app/widgets/chat_widget.dart';
+import 'package:chatgpt_app/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
@@ -39,7 +40,30 @@ class _ChatScreenState extends State<ChatScreen> {
         ),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () async {
+              await showModalBottomSheet(
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(20),
+                  )),
+                  backgroundColor: scaffoldBackgroundColor,
+                  context: context,
+                  builder: (context) {
+                    return const Padding(
+                      padding: EdgeInsets.all(18.0),
+                      child: Row(
+                        children: [
+                          Flexible(
+                            child: TextWidget(
+                              label: "Chosen Model:",
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  });
+            },
             icon: const Icon(
               Icons.more_vert_rounded,
               color: Colors.white,
