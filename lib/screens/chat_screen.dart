f9709b41382faf014f4a8581437896543f0e8a54
avikumarr355@gmail.com <avikumarr355@gmail.com>
2024-01-03 01:04:41 +0530

    create services class

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:chatgpt_app/constants/constants.dart';
 import 'package:chatgpt_app/services/assets_manager.dart';
+import 'package:chatgpt_app/services/services.dart';
 import 'package:chatgpt_app/widgets/chat_widget.dart';
 import 'package:chatgpt_app/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
@@ -41,28 +44,7 @@ class _ChatScreenState extends State<ChatScreen> {
         actions: [
           IconButton(
             onPressed: () async {
-              await showModalBottomSheet(
-                  shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(20),
-                  )),
-                  backgroundColor: scaffoldBackgroundColor,
-                  context: context,
-                  builder: (context) {
-                    return const Padding(
-                      padding: EdgeInsets.all(18.0),
-                      child: Row(
-                        children: [
-                          Flexible(
-                            child: TextWidget(
-                              label: "Chosen Model:",
-                              fontSize: 16,
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  });
+              await Services.showModalSheet(context: context);
             },
             icon: const Icon(
               Icons.more_vert_rounded,
